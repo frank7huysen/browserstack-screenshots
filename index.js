115d31020011ef9960d6442ffd5096a248261ee1
Frank <franksevenhuysen@hotmail.com>
2020-09-28 14:20:57 +0200

    simple log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
 console.log('Browserstack Screenshot Api CI/CD');
 
