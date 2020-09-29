7899eda75fd5b2aa1368afe80d6be34989b5ba35
Frank <franksevenhuysen@hotmail.com>
2020-09-29 10:57:22 +0200

    make screenshot

--- index.js
+++ index.js
@@ -3,20 +3,76 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const BrowserStack = require("browserstack");
 
+
+const generateScreenshotsAsync = (screenshotClient) => {
+  return new Promise((resolve, reject) => {
+
+      screenshotClient.generateScreenshots({
+          "url": "https://staging.try.theaterthuis.nl/nl-nl/",
+          "win_res": "1024x768",
+          "mac_res": "1920x1080",
+          "quality": "compressed",
+          "wait_time": 15,
+          "orientation": "portrait",
+          "browsers": [
+              {
+                  "os": "Windows",
+                  "os_version": "10",
+                  "browser": "chrome",
+                  "browser_version": "71.0"
+              },
+              {
+                  "os": "ios",
+                  "os_version": "14",
+                  "browser": "Mobile Safari",
+                  "device": "iPhone 11",
+                  "browser_version": null,
+                  "real_mobile": true
+              },
+              {
+                  "os": "ios",
+                  "os_version": "13",
+                  "browser": "Mobile Safari",
+                  "device": "iPhone SE 2020",
+                  "browser_version": null,
+                  "real_mobile": true
+              },
+              {
+                  "os": "android",
+                  "os_version": "10.0",
+                  "browser": "Android Browser",
+                  "device": "Samsung Galaxy S20",
+                  "browser_version": null,
+                  "real_mobile": true
+              },
+          ]
+      }, (error, job) => {
+          if (error) {
+              return reject(error)
+          }
+          return resolve(job);
+      });
+  });
+};
+
 (async () => {
-  
+
   try {
-    core.info('Start making screenshots');
-    core.info('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
-    core.info('Current dir: ', path.dirname(__filename));
+    console.log('Start making screenshots');
+    console.log('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
+    console.log('Current dir: ', path.dirname(__filename));
   
     // Setup browserstack screenshot client
     const browserStackCredentials = {
       username: process.env.BROWSERSTACK_USERNAME,
       password: process.env.BROWSERSTACK_PASSWORD,
     };  
+
     const screenshotClient = await BrowserStack.createScreenshotClient(browserStackCredentials);
-    core.info('screenshotClient: ', JSON.stringify(screenshotClient, null, 4));
+    const screenshots = await generateScreenshotsAsync(screenshotClient);
+    console.log("screenshots: ", screenshots);
+
+
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
