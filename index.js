5eef468711d42d22acf11d224ba480b4d421a922
Frank <franksevenhuysen@hotmail.com>
2020-09-29 11:30:32 +0200

    pass screenshot config

--- index.js
+++ index.js
@@ -8,35 +8,76 @@ const generateScreenshotsAsync = require('./src/browserstack/generate-screenshot
 const waitForJobToFinishAsync = require('./src/browserstack/wait-for-job-to-finish-async');
 
 
-(async () => {
+const screenshotConfig = {
+  "url": "https://staging.try.theaterthuis.nl/nl-nl/",
+  "win_res": "1024x768",
+  "mac_res": "1920x1080",
+  "quality": "compressed",
+  "wait_time": 15,
+  "orientation": "portrait",
+  "browsers": [
+    {
+      "os": "Windows",
+      "os_version": "10",
+      "browser": "chrome",
+      "browser_version": "71.0"
+    },
+    {
+      "os": "ios",
+      "os_version": "14",
+      "browser": "Mobile Safari",
+      "device": "iPhone 11",
+      "browser_version": null,
+      "real_mobile": true
+    },
+    {
+      "os": "ios",
+      "os_version": "13",
+      "browser": "Mobile Safari",
+      "device": "iPhone SE 2020",
+      "browser_version": null,
+      "real_mobile": true
+    },
+    {
+      "os": "android",
+      "os_version": "10.0",
+      "browser": "Android Browser",
+      "device": "Samsung Galaxy S20",
+      "browser_version": null,
+      "real_mobile": true
+    },
+  ]
+};
 
-  try {
-    console.log('Start making screenshots');
-    console.log('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
-    console.log('Current dir: ', path.dirname(__filename));
+  (async () => {
 
-    // Setup browser stack screenshot client
-    const browserStackCredentials = {
-      username: process.env.BROWSERSTACK_USERNAME,
-      password: process.env.BROWSERSTACK_PASSWORD,
-    };
+    try {
+      console.log('Start making screenshots');
+      console.log('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
+      console.log('Current dir: ', path.dirname(__filename));
 
-    const screenshotClient = await BrowserStack.createScreenshotClient(browserStackCredentials);
-    const screenshotsJob = await generateScreenshotsAsync(screenshotClient);
-    const finishedJob = await waitForJobToFinishAsync(screenshotClient, screenshotsJob.job_id);
+      // Setup browser stack screenshot client
+      const browserStackCredentials = {
+        username: process.env.BROWSERSTACK_USERNAME,
+        password: process.env.BROWSERSTACK_PASSWORD,
+      };
 
-    console.log("finishedJob: ", JSON.stringify(finishedJob, null, 4));
+      const screenshotClient = await BrowserStack.createScreenshotClient(browserStackCredentials);
+      const screenshotsJob = await generateScreenshotsAsync(screenshotClient, screenshotConfig);
+      const finishedJob = await waitForJobToFinishAsync(screenshotClient, screenshotsJob.job_id);
 
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+      console.log("finishedJob: ", JSON.stringify(finishedJob, null, 4));
 
-})();
+      // `who-to-greet` input defined in action metadata file
+      const nameToGreet = core.getInput('who-to-greet');
+      console.log(`Hello ${nameToGreet}!`);
+      const time = (new Date()).toTimeString();
+      core.setOutput("time", time);
+      // Get the JSON webhook payload for the event that triggered the workflow
+      const payload = JSON.stringify(github.context.payload, undefined, 2)
+      console.log(`The event payload: ${payload}`);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+
+  })();
