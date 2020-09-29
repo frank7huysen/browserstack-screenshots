ad764e3f400b51990a6a76b2d37f15ec5de4a8d8
Frank <franksevenhuysen@hotmail.com>
2020-09-29 11:23:21 +0200

    start screenshot job

--- index.js
+++ index.js
@@ -3,57 +3,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const BrowserStack = require("browserstack");
 
+// Utils
+const generateScreenshotsAsync = require('./src/browserstack/generate-screenshots-async');
+const waitForJobToFinishAsync = required('./src/browserstack/wait-for-job-to-finish-async');
 
-const generateScreenshotsAsync = (screenshotClient) => {
-  return new Promise((resolve, reject) => {
-
-      screenshotClient.generateScreenshots({
-          "url": "https://staging.try.theaterthuis.nl/nl-nl/",
-          "win_res": "1024x768",
-          "mac_res": "1920x1080",
-          "quality": "compressed",
-          "wait_time": 15,
-          "orientation": "portrait",
-          "browsers": [
-              {
-                  "os": "Windows",
-                  "os_version": "10",
-                  "browser": "chrome",
-                  "browser_version": "71.0"
-              },
-              {
-                  "os": "ios",
-                  "os_version": "14",
-                  "browser": "Mobile Safari",
-                  "device": "iPhone 11",
-                  "browser_version": null,
-                  "real_mobile": true
-              },
-              {
-                  "os": "ios",
-                  "os_version": "13",
-                  "browser": "Mobile Safari",
-                  "device": "iPhone SE 2020",
-                  "browser_version": null,
-                  "real_mobile": true
-              },
-              {
-                  "os": "android",
-                  "os_version": "10.0",
-                  "browser": "Android Browser",
-                  "device": "Samsung Galaxy S20",
-                  "browser_version": null,
-                  "real_mobile": true
-              },
-          ]
-      }, (error, job) => {
-          if (error) {
-              return reject(error)
-          }
-          return resolve(job);
-      });
-  });
-};
 
 (async () => {
 
@@ -61,17 +14,18 @@ const generateScreenshotsAsync = (screenshotClient) => {
     console.log('Start making screenshots');
     console.log('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
     console.log('Current dir: ', path.dirname(__filename));
-  
-    // Setup browserstack screenshot client
+
+    // Setup browser stack screenshot client
     const browserStackCredentials = {
       username: process.env.BROWSERSTACK_USERNAME,
       password: process.env.BROWSERSTACK_PASSWORD,
-    };  
+    };
 
     const screenshotClient = await BrowserStack.createScreenshotClient(browserStackCredentials);
-    const screenshots = await generateScreenshotsAsync(screenshotClient);
-    console.log("screenshots: ", screenshots);
+    const screenshotsJob = await generateScreenshotsAsync(screenshotClient);
+    const finishedJob = await waitForJobToFinishAsync(screenshotClient, screenshotsJob.job_id);
 
+    console.log("finishedJob: ", JSON.stringify(finishedJob, null, 4));
 
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
