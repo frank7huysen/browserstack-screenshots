d706966d3d348b20ec83be200c14c6cc55f4a429
Frank <franksevenhuysen@hotmail.com>
2020-10-07 16:00:21 +0200

    config dir

--- src/action-handlers/generate-screenshots.js
+++ src/action-handlers/generate-screenshots.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const BrowserStack = require("browserstack");
+const BrowserStack = require('browserstack');
 const core = require('@actions/core');
 
 // Utils
@@ -21,27 +21,40 @@ const mergeConfigPerUrl = (screenshotUrls, baseConfig) => {
     config.url = url;
     return config;
   });
-}
+};
 
 const generateScreenshots = async () => {
-  let screenshotUrls = process.env.NODE_ENV !== 'development'
-    ? core.getInput('screenshot-page-urls')
-    : process.env.SCREENSHOT_PAGE_URLS;
+  let screenshotUrls =
+    process.env.NODE_ENV !== 'development'
+      ? core.getInput('screenshot-page-urls')
+      : process.env.SCREENSHOT_PAGE_URLS;
+
+  const configDir =
+    process.env.NODE_ENV !== 'development'
+      ? appDir
+      : process.env.GITHUB_WORKSPACE;
   screenshotUrls = screenshotUrls.split(',');
 
   console.log('Start making screenshots');
+  console.log('Config dir: ', configDir);
   console.log('Browserstack user: ', process.env.BROWSERSTACK_USERNAME);
   console.log('Screenshot urls: ', screenshotUrls);
 
   if (!screenshotUrls || !screenshotUrls.length) {
     throw new Error('No screenshot urls found');
   }
-  if (!process.env.BROWSERSTACK_USERNAME || !process.env.BROWSERSTACK_PASSWORD) {
+  if (
+    !process.env.BROWSERSTACK_USERNAME ||
+    !process.env.BROWSERSTACK_PASSWORD
+  ) {
     throw new Error('Browserstack USERNAME or PASSWORD not set');
   }
 
-  const baseConfig = await readBrowserConfigAsync(appDir);
-  if (!baseConfig) throw new Error('Config not found at ./screenshot-flow/browser-settings.json');
+  const baseConfig = await readBrowserConfigAsync(configDir);
+  if (!baseConfig)
+    throw new Error(
+      'Config not found at ./screenshot-flow/browser-settings.json',
+    );
 
   const screenshotConfigs = mergeConfigPerUrl(screenshotUrls, baseConfig);
   console.log(screenshotConfigs);
@@ -51,16 +64,24 @@ const generateScreenshots = async () => {
   for await (const screenshotConfig of screenshotConfigs) {
     console.log('MAKE SCREENSHOT FOR PAGE: ', screenshotConfig);
 
-    const screenshotClient = await BrowserStack.createScreenshotClient(browserStackCredentials);
-    const screenshotsJob = await generateScreenshotsAsync(screenshotClient, screenshotConfig);
-    const finishedJob = await waitForJobToFinishAsync(screenshotClient, screenshotsJob.job_id);
+    const screenshotClient = await BrowserStack.createScreenshotClient(
+      browserStackCredentials,
+    );
+    const screenshotsJob = await generateScreenshotsAsync(
+      screenshotClient,
+      screenshotConfig,
+    );
+    const finishedJob = await waitForJobToFinishAsync(
+      screenshotClient,
+      screenshotsJob.job_id,
+    );
 
     finishedJobs.push(finishedJob);
     console.log('FiNISHED SCREENSHOT FOR PAGE: ', screenshotConfig);
   }
 
-  console.log("finishedJob: ", JSON.stringify(finishedJobs, null, 4));
-  core.setOutput("job-result", finishedJobs);
-}
+  console.log('finishedJob: ', JSON.stringify(finishedJobs, null, 4));
+  core.setOutput('job-result', finishedJobs);
+};
 
 module.exports = generateScreenshots;
